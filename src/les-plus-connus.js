0ef3d4235bb166c30472ba725f87a70e0801deff
joelgaillard <147040741+joelgaillard@users.noreply.github.com>
2024-03-21 15:43:03 +0100

    Continue mise en place

---
+++ src/les-plus-connus.js
@@ -0,0 +1,8 @@
+
+
+<div class="photo-item">
+    <a href="lien_image1.html" class="photo-link">
+      <img src="image1.jpg" alt="Photo 1" style="max-width: 100%;" width="100%">
+      <div class="photo-title">Titre de la photo 1</div>
+    </a>
+  </div>
